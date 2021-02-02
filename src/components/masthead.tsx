50f7f98b663a2bed501c9bbe27e04df15d405f34
James <>
2021-02-01 16:15:02 -0800

    Minor adjustments

--- src/components/masthead.tsx
+++ src/components/masthead.tsx
@@ -7,12 +7,6 @@ function Masthead() {
       <img src="./masthead.png" width="180" alt="OTV Dashboard" />
       <div className="moon"></div>
       <div className="cloud1"></div>
-
-      {/* <h1> */}
-        {/* <span className="header">OFFLINE TV</span>
-        <span className="subheader">& FRIENDS!</span> */}
-      {/* </h1> */}
-
       <span className="cloud2"></span>
     </div>
   );
