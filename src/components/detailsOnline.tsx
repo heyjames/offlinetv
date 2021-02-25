bfc3077f4f33d6010543713b4306222719800047
James <>
2021-02-24 19:10:52 -0800

    Revert "Change channel title to ellipsis on small widths"
    This reverts commit a79155b2199806526174fe0b50848329bf8d178d.

--- src/components/detailsOnline.tsx
+++ src/components/detailsOnline.tsx
@@ -18,7 +18,7 @@ const DetailsOnline: React.FC<DetailsOnlineProps> = ({ member }) => {
         <Name member={member} />
 
         <div className="stream-title" title={title}>
-          <span>{live && title}</span>
+          {live && title}
         </div>
 
         <div className="view-count" title="View Count">
