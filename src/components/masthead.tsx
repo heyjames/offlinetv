dfcae0b154d5a5d11446958bf1e70fb8657996d4
James <>
2021-02-25 02:42:29 -0800

    Add small masthead

--- src/components/masthead.tsx
+++ src/components/masthead.tsx
@@ -1,13 +1,23 @@
+import React from 'react';
 import '../App.css';
 
 function Masthead() {
   return (
-    <div className="masthead">
-      <img src="/masthead.png" width="180" alt="OTV Dashboard" />
-      <div className="moon"></div>
-      <div className="cloud1"></div>
-      <span className="cloud2"></span>
-    </div>
+    <React.Fragment>
+      <div className="masthead">
+        <img src="/masthead.png" width="180" alt="OTV Dashboard" />
+        <div className="moon"></div>
+        <div className="cloud1"></div>
+        <span className="cloud2"></span>
+      </div>
+      
+      <div className="masthead-sm-p">
+        <div className="masthead-sm">
+          <span className="favicon-p"><img src="/favicon.ico" width="34" alt="OTV Dashboard" /></span>
+          <span className="site-name">OTV Dashboard</span>
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
